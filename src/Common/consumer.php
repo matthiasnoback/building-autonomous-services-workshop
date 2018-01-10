46cc853bab12fc713049401902b6085eea0dd54e
Matthias Noback <matthiasnoback@gmail.com>
2018-01-10 20:40:14 +0100

    Add a 'consume everything' consumer

---
+++ src/Common/consumer.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+use Common\Stream\Stream;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+/*
+ * This is a demo consumer which consumes every message from the stream.
+ * It prints the message to stdout. Hence, if you want to visually keep
+ * track of the stream, run:
+ *
+ *   docker-compose logs -f consumer
+ */
+Stream::consume(function(string $messageType, $data) {
+    echo $messageType . ': ' . json_encode($data) . "\n";
+});
