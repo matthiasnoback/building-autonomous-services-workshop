9a7ba8a3bfaa477d2a79144bfc791335098e55f1
Matthias Noback <matthiasnoback@gmail.com>
2018-06-06 15:52:21 +0200

    Remove integration test suite

--- run_tests.sh
+++ run_tests.sh
@@ -4,5 +4,4 @@ set -eu
 
 vendor/bin/phpstan analyze --level=max src test
 vendor/bin/phpunit --testsuite unit -vvv --stop-on-error
-vendor/bin/phpunit --testsuite integration -vvv --stop-on-error
 vendor/bin/behat --suite system -vvv --stop-on-failure
