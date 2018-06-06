47a1462a009974a8dc850badac8212a9cdbd996b
Matthias Noback <matthiasnoback@gmail.com>
2018-06-06 11:13:07 +0200

    Solution to assignment 02

--- src/Stock/StockApplication.php
+++ src/Stock/StockApplication.php
@@ -3,40 +3,15 @@ declare(strict_types=1);
 
 namespace Stock;
 
+use Common\Persistence\Database;
 use Common\Render;
-use Common\Web\HttpApi;
 
 final class StockApplication
 {
     public function stockLevelsController(): void
     {
-        $stockLevels = $this->calculateStockLevels();
+        $stockLevels = Database::retrieveAll(Balance::class);
 
         Render::jsonOrHtml($stockLevels);
-    }
-
-    private function calculateStockLevels(): array
-    {
-        $stockLevels = [];
-
-        $purchaseOrders = HttpApi::fetchDecodedJsonResponse('http://purchase_web/listPurchaseOrders');
-        foreach ($purchaseOrders as $purchaseOrder) {
-            if (!$purchaseOrder->received) {
-                continue;
-            }
-
-            $stockLevels[$purchaseOrder->productId] = ($stockLevels[$purchaseOrder->productId] ?? 0) + $purchaseOrder->quantity;
-        }
-
-        $salesOrders = HttpApi::fetchDecodedJsonResponse('http://sales_web/listSalesOrders');
-        foreach ($salesOrders as $salesOrder) {
-            if (!$salesOrder->wasDelivered) {
-                continue;
-            }
-
-            $stockLevels[$salesOrder->productId] = ($stockLevels[$salesOrder->productId] ?? 0) - $salesOrder->quantity;
-        }
-
-        return $stockLevels;
     }
 }
---
+++ src/Stock/process_manager.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+use Common\Persistence\Database;
+use Common\Persistence\KeyValueStore;
+use Common\Stream\Stream;
+use Stock\Balance;
+use Symfony\Component\Debug\Debug;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+Debug::enable();
+
+$startAtIndexKey = $startAtIndexKey = basename(__DIR__) . '_start_at_index';
+
+$startAtIndex = KeyValueStore::get($startAtIndexKey) ?: 0;
+echo 'Start consuming at index: ' . (string)$startAtIndex;
+
+Stream::consume(
+    function (string $messageType, $data) use ($startAtIndexKey) {
+        if ($messageType === 'catalog.product_created') {
+            /** @var Balance $balance */
+            $balance = new Balance($data['productId']);
+            Database::persist($balance);
+        }
+        elseif ($messageType === 'purchase.goods_received') {
+            /** @var Balance $balance */
+            $balance = Database::retrieve(Balance::class, $data['productId']);
+            $balance->increase($data['quantity']);
+            Database::persist($balance);
+        }
+        elseif ($messageType === 'sales.goods_delivered') {
+            /** @var Balance $balance */
+            $balance = Database::retrieve(Balance::class, $data['productId']);
+            $balance->decrease($data['quantity']);
+            Database::persist($balance);
+        }
+
+        KeyValueStore::incr($startAtIndexKey);
+    },
+    $startAtIndex
+);
