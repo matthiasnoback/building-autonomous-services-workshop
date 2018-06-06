4943d137f441f90e24db0518fd2fb782c7f9c649
Matthias Noback <matthiasnoback@gmail.com>
2018-06-06 13:56:29 +0200

    Solution to assignment 07

--- src/Sales/OrderStatus.php
+++ src/Sales/OrderStatus.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Sales;
 
+use Assert\Assertion;
 use Common\Persistence\IdentifiableObject;
 
 /**
@@ -10,6 +11,11 @@ use Common\Persistence\IdentifiableObject;
  */
 final class OrderStatus implements IdentifiableObject
 {
+    private const SALES_ORDER_CREATED = 'sales_order_created';
+    private const AWAITING_STOCK_RESERVATION = 'awaiting_stock_reservation';
+    private const AWAITING_GOODS_RECEIVED = 'awaiting_goods_received';
+    private const SALES_ORDER_DELIVERED = 'sales_order_delivered';
+
     /**
      * @var string
      */
@@ -20,9 +26,37 @@ final class OrderStatus implements IdentifiableObject
      */
     private $purchaseOrderId;
 
+    /**
+     * @var string
+     */
+    private $status;
+
     public function __construct(string $salesOrderId)
     {
         $this->salesOrderId = $salesOrderId;
+        $this->status = self::SALES_ORDER_CREATED;
+    }
+
+    public function awaitingStockReservation(): void
+    {
+        $this->assertStatusIn([self::SALES_ORDER_CREATED, self::AWAITING_GOODS_RECEIVED]);
+
+        $this->status = self::AWAITING_STOCK_RESERVATION;
+    }
+
+    public function awaitingGoodsReceived(string $purchaseOrderId): void
+    {
+        $this->assertStatusIn([self::AWAITING_STOCK_RESERVATION]);
+
+        $this->purchaseOrderId = $purchaseOrderId;
+        $this->status = self::AWAITING_GOODS_RECEIVED;
+    }
+
+    public function salesOrderDelivered(): void
+    {
+        $this->assertStatusIn([self::AWAITING_STOCK_RESERVATION]);
+
+        $this->status = self::SALES_ORDER_DELIVERED;
     }
 
     public function id(): string
@@ -30,13 +64,13 @@ final class OrderStatus implements IdentifiableObject
         return $this->salesOrderId;
     }
 
-    public function setPurchaseOrderId(string $purchaseOrderId): void
-    {
-        $this->purchaseOrderId = $purchaseOrderId;
-    }
-
     public function purchaseOrderId(): ?string
     {
         return $this->purchaseOrderId;
+    }
+
+    private function assertStatusIn(array $possibleStatuses): void
+    {
+        Assertion::inArray($this->status, $possibleStatuses);
     }
 }
