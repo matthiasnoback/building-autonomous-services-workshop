2fb3b0c26edfb15e66bba00be9d080967074d1d5
Matthias Noback <matthiasnoback@gmail.com>
2018-06-06 10:45:22 +0200

    Solution to assignment 01

--- src/Purchase/PurchaseApplication.php
+++ src/Purchase/PurchaseApplication.php
@@ -6,7 +6,6 @@ namespace Purchase;
 use Common\Persistence\Database;
 use Common\Render;
 use Common\Web\FlashMessage;
-use Common\Web\HttpApi;
 
 final class PurchaseApplication
 {
@@ -32,7 +31,8 @@ final class PurchaseApplication
             exit;
         }
 
-        $products = array_values((array)HttpApi::fetchDecodedJsonResponse('http://catalog_web/listProducts'));
+        /** @var Product[] $products */
+        $products = Database::retrieveAll(Product::class);
 
         include __DIR__ . '/../Common/header.php';
 
@@ -47,7 +47,7 @@ final class PurchaseApplication
                     <?php
                     foreach ($products as $product) {
                         ?>
-                        <option value="<?php echo $product->productId; ?>"><?php echo htmlspecialchars($product->name); ?></option>
+                        <option value="<?php echo $product->id(); ?>"><?php echo htmlspecialchars($product->name()); ?></option>
                         <?php
                     }
                     ?>
---
+++ src/Purchase/projector.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+use Common\Persistence\Database;
+use Common\Stream\Stream;
+use Purchase\Product;
+use Symfony\Component\ErrorHandler\Debug;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+Debug::enable();
+
+Database::deleteAll(Product::class);
+
+Stream::consume(function (string $messageType, $data) {
+    if ($messageType === 'catalog.product_created') {
+        $product = new Product($data['productId'], $data['name']);
+        Database::persist($product);
+    }
+});
