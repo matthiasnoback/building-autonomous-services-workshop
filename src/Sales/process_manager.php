0c33a105849c821ce0ab6630663ded2e1d8cf804
Matthias Noback <matthiasnoback@gmail.com>
2018-06-06 13:38:53 +0200

    Solution to assignment 06

--- src/Sales/process_manager.php
+++ src/Sales/process_manager.php
@@ -3,9 +3,12 @@ declare(strict_types=1);
 
 use function Common\CommandLine\line;
 use function Common\CommandLine\stdout;
+use Common\Persistence\Database;
 use Common\Persistence\KeyValueStore;
 use Common\Stream\Stream;
 use Common\Web\HttpApi;
+use Ramsey\Uuid\Uuid;
+use Sales\OrderStatus;
 use Symfony\Component\Debug\Debug;
 
 require __DIR__ . '/../../vendor/autoload.php';
@@ -20,6 +23,9 @@ stdout(line('Start consuming at index', ':', (string)$startAtIndex));
 Stream::consume(
     function (string $messageType, $data) use ($startAtIndexKey) {
         if ($messageType === 'sales.sales_order_created') {
+            $orderStatus = new OrderStatus($data->salesOrderId);
+            Database::persist($orderStatus);
+
             echo HttpApi::postFormData(
                 'http://stock_web/makeStockReservation',
                 [
@@ -35,6 +41,37 @@ Stream::consume(
                     'salesOrderId' => $data->reservationId
                 ]
             );
+        } elseif ($messageType === 'stock.reservation_rejected') {
+            /** @var OrderStatus $orderStatus */
+            $orderStatus = Database::retrieve(OrderStatus::class, $data->reservationId);
+            $purchaseOrderId = Uuid::uuid4()->toString();
+            $orderStatus->setPurchaseOrderId($purchaseOrderId);
+            Database::persist($orderStatus);
+
+            echo HttpApi::postFormData(
+                'http://purchase_web/createPurchaseOrder',
+                [
+                    'purchaseOrderId' => $purchaseOrderId,
+                    'productId' => $data->productId,
+                    'quantity' => $data->quantity
+                ]
+            );
+        } elseif ($messageType === 'stock.stock_level_increased') {
+            $purchaseOrderId = $data->correlationId;
+
+            $orderStatus = Database::findOne(OrderStatus::class, function (OrderStatus $orderStatus) use ($purchaseOrderId) {
+                return $orderStatus->purchaseOrderId() === $purchaseOrderId;
+            });
+            if ($orderStatus instanceof OrderStatus) {
+                echo HttpApi::postFormData(
+                    'http://stock_web/makeStockReservation',
+                    [
+                        'reservationId' => $orderStatus->id(), // the sales order ID
+                        'productId' => $data->productId, // the product that was just received
+                        'quantity' => $data->quantity // the quantity that was just received
+                    ]
+                );
+            }
         } elseif ($messageType === 'sales.goods_delivered') {
             echo HttpApi::postFormData(
                 'http://stock_web/commitStockReservation',
