a776a2549bc8ef9b10f5c57e6f3aa7b615ddb05e
Matthias Noback <matthiasnoback@gmail.com>
2019-11-15 15:54:11 +0100

    Only try to deliver the order inside the assertEventually() callback
    Otherwise we might be creating many orders as well.

--- test/System/FeatureContext.php
+++ test/System/FeatureContext.php
@@ -94,12 +94,12 @@ final class FeatureContext extends MinkContext
      */
     public function weHaveSoldAndDeliveredItemsOfThisProduct(string $quantity): void
     {
-        self::assertEventually(function () use ($quantity) {
-            $this->visit('http://sales.localhost/createSalesOrder');
-            $this->selectOption('Product', $this->product);
-            $this->fillField('Quantity', $quantity);
-            $this->pressButton('Order');
+        $this->visit('http://sales.localhost/createSalesOrder');
+        $this->selectOption('Product', $this->product);
+        $this->fillField('Quantity', $quantity);
+        $this->pressButton('Order');
 
+        self::assertEventually(function () {
             $this->visit('http://sales.localhost/deliverSalesOrder');
             $this->pressButton('Deliver');
         });
