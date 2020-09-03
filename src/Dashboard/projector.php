90ce1212dfddf89b5c1de8b4787bf3ddcdccb6cb
Matthias Noback <matthiasnoback@gmail.com>
2020-09-03 14:00:29 +0200

    Solution to assignment 03

---
+++ src/Dashboard/projector.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+use Common\Persistence\Database;
+use Common\Stream\Stream;
+use Dashboard\Product;
+use Symfony\Component\ErrorHandler\Debug;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+Debug::enable();
+
+Database::deleteAll(Product::class);
+
+Stream::consume(function(string $messageType, $data) {
+    if ($messageType === 'catalog.product_created') {
+        $product = new Product($data['productId'], $data['name']);
+        Database::persist($product);
+    }
+    elseif ($messageType === 'stock.stock_level_changed') {
+        /** @var Product $product */
+        $product = Database::retrieve(Product::class, $data['productId']);
+        $product->setStockLevel($data['stockLevel']);
+        Database::persist($product);
+    }
+});
